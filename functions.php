ccc220fc0cb3c18de1570a973fd866d76cea2b6e
Ben Rowell <61505096+TPBRowell93@users.noreply.github.com>
2020-03-31 14:55:35 +0100

    Add files via upload

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ session_start();
 // connect to database
 function getConnection() {
    $db = new PDO(
- "mysql:host=localhost;dbname=NEED DATABASE NAME",
- 'unn_USERNAME', 'PASSWORD');
+ "mysql:host=localhost; dbname=unn_w19032995",
+ 'unn_w19032995', 'TeamProject2020');
    return $db;
 }
-
+	
 // variable declaration
 $username = "";
 $email    = "";
