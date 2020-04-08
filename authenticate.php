373beb85cae4e1f6620e3dadda373f532152df80
w19034959 <61505344+w19034959@users.noreply.github.com>
2020-04-08 16:52:41 +0100

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,5 @@
 <?php
-// get values from form 
+// get values from form
 
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -11,24 +11,17 @@ $db = getConnection();
 $sql = ("SELECT * from TPUsers where email = '$email' and password = '$password' and permissions = '$role'");
 $results = $db->query($sql);
 
-$row = $results-> fetchObject(); 
+$row = $results-> fetchObject();
 
 if ($row->email == $email && $row->password == $password && $row->permissions == "Management"){
-	echo header('Location: http://unn-w19032995.newnumyspace.co.uk/TeamProject/ManagerMenu.php');
+	echo header('Location: http://unn-w19034959.newnumyspace.co.uk/ManagerMenu.php');
 }
 
 else if ($row->email == $email && $row->password == $password && $row->permissions == "Staff"){
-	echo header('Location: http://unn-w19032995.newnumyspace.co.uk/TeamProject/StaffMenu.php');
+	echo header('Location: http://unn-w19034959.newnumyspace.co.uk/StaffMenu.php');
 }
 
 else {
-	echo header('Location: http://unn-w19032995.newnumyspace.co.uk/TeamProject/LandingPage.php');
+	echo header('Location: http://unn-w19034959.newnumyspace.co.uk/LandingPage.php');
 }
 
-
-
-
-
-
-
-?>
