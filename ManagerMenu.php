7bb6fc046b0451af505ef48458e9ea238711001c
w19034959 <61505344+w19034959@users.noreply.github.com>
2020-04-08 16:51:23 +0100

    Update ManagerMenu.php

--- ManagerMenu.php
+++ ManagerMenu.php
@@ -1,13 +1,13 @@
 <?php include('functions.php');
 $db = getConnection();
  ?>
-
+ 
 <!DOCTYPE html>
 <html lang="en">
 		<head>
 				<meta charset="utf-8">
-				<title>Staff Portal</title>
-				<link rel="stylesheet" href="CSS/StaffMenu.css">
+				<title>Manager Portal</title>
+				<link rel="stylesheet" href="CSS/ManagerMenu.css">
 		</head>
 		<body>
                 <div class="bg-img">
@@ -15,10 +15,11 @@ $db = getConnection();
 							<img id = "logo" img src="Images/logo.jpg">
 							<ul>
 								<li><a href="#" class = "active">Home</a></li>
-								<li><a href="#">View Rotas</a></li>
-								<li><a href="#">Request Day Off</a></li>
-								<li><a href="#">Request Holiday</a></li>
-								<li><a href="#">Log Out</a></li>
+								<li><a href="CreateRota.php">Create Rota</a></li>
+								<li><a href="#">Alter Rota</a></li>
+								<li><a href="#">Requests</a></li>
+								<li><a href="admin.php" class = "active">Admin</a></li>
+								<li><a href="logout.php">Log Out</a></li>
 							</ul>
 					</div>
 				</div>
