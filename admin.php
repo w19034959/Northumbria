ecbd9d122cf6854586b15dcae291bcfd7f82cd88
w19034959 <61505344+w19034959@users.noreply.github.com>
2020-04-12 17:36:01 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -22,25 +22,50 @@ $db = getConnection();
 								<li><a href="logout.php">Log Out</a></li>
 							</ul>
 					</div>
-				
-    <form action="action_page.php" style="border:1px solid #ccc">
+
+    <form action="addstaff.php" method ="post" style="border:1px solid #ccc">
       <div class="container">
         <h1>Sign Up</h1>
         <p>Please fill in this form to create an account.</p>
         <hr>
 
+        <label for="forename"><b>Forename</b></label>
+        <input type="text" placeholder="Enter Forename" name="forename" required>
+
+        <label for="surname"><b>Surname</b></label>
+        <input type="text" placeholder="Enter Surname" name="surname" required>
+
         <label for="email"><b>Email</b></label>
         <input type="text" placeholder="Enter Email" name="email" required>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
 
         <label for="psw-repeat"><b>Repeat Password</b></label>
         <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
 
-        <label>
-        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
+        <label for="address"><b>Address</b></label>
+        <input type="text" placeholder="Enter Address" name="address" required>
+
+        <label for="phone_number"><b>Number</b></label>
+        <input for="text" placehoder="Enter Number" name="phone_number" required>
+
+        <label for="department">Select Department</label><br>
+        <select name="department">
+          <option value="Staff">Staff</option>
+          <option value="Management">Management</option>
+		  <option value="Kitchen">Kitchen</option>
+        </select>
+
+		<label for="permissions">Select Permissions</label><br>
+        <select name="permissions">
+          <option value="Staff">Staff</option>
+          <option value="Management">Management</option>
+        </select>
+
+        <label><br><br>
+        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px">Remember me
+        </label>
 
     <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
@@ -50,6 +75,7 @@ $db = getConnection();
     </div>
   </div>
   </div>
+
 </form>
 		</body>
 </html>
